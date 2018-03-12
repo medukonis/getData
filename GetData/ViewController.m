ee7e5083f809a4472dedb295db3a3ae106505a85
DVCUser <dvcuser@F36232.local>
2018-03-12 08:58:56 -0400

    Initial Commit

---
+++ GetData/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GetData
+//
+//  Created by DVCUser on 3/12/18.
+//  Copyright © 2018 DVCUser. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ GetDataTests/GetDataTests.m
@@ -0,0 +1,39 @@
+//
+//  GetDataTests.m
+//  GetDataTests
+//
+//  Created by DVCUser on 3/12/18.
+//  Copyright © 2018 DVCUser. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GetDataTests : XCTestCase
+
+@end
+
+@implementation GetDataTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
